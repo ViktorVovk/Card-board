a6f6882ae2394ac391243612c626f05722761fae
ViktorVovk <34575602+ViktorVovk@users.noreply.github.com>
2018-09-19 19:53:46 +0300

    Delete BtnColorPiker.js

--- src/component/BtnColorPiker/BtnColorPiker.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const BtnColorPiker = ({clickBtnColorPiker}) => <button className="card__btn-color-piker" onClick={clickBtnColorPiker} title="Change Color"></button>
-
-export default BtnColorPiker
